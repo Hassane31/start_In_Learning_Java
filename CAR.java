1731311d185c26f3934d1146f0a55c3ba27f07f6
Hassane31 <hassane.laabane@gmail.com>
2025-03-12 10:39:30 +0000

    madrt wlw

--- CAR.java
+++ CAR.java
@@ -1,22 +1,52 @@
-public class CAR {
-   private String name;
-   private int maxSpeed;
-   private float price;
-   private int model ;
+import java.util.Scanner;
 
-    public void setname(String n){
-        name = n;
+public class CAR {
+    Scanner input = new Scanner(System.in);
+
+    private String name;
+    private int maxSpeed;
+    private float price;
+    private int model;
+
+    public void setname() {
+        System.out.println("car name :");
+        name = input.next();}
+public String getname() {
+        return name; }
+
+
+    public void setModel() {
+       System.out.println("car model");
+        model = input.nextInt();
+        if (model >= 2000 && model <= 2024)
+            System.out.println("nice model:");
+        else{
+        System.out.println("sorry we can t accepte this car");
+        do{System.out.println("Again car model :");
+        model = input.nextInt();}while(model > 2000 && model <2024);
+        }}
+    public int getModel() {
+        return model;}
+
+
+    public void setMaxSpeed() {
+        do {  System.out.println("car maxSpeed :");
+            maxSpeed = input.nextInt();
+        } while (maxSpeed < 0);
+        if (maxSpeed >= 180)
+            System.out.println("Speed car!!");
+        else
+            System.out.println("normal car !!");  }
+    public int getMaxSpeed() {
+        return maxSpeed;
     }
-    public String getname (){
-        return name ;
+
+
+    public void setPrice() {
+        do {System.out.println("price : !!");
+            price = input.nextFloat();
+        } while (price <= 0); }
+    public float getPrice() {
+        return price;
     }
-    public void setModel(int n) {
-        if (n>=1994)
-        model = n;
-        else System.out.println("sorry we can t accepte this car");
-    }
-     int getModel (){
-        return model ;
-    }
-    
 }
--- mainclass.java
+++ mainclass.java
@@ -1,12 +1,16 @@
-
+import java.util.Scanner;
 public class mainclass {
+  Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
       CAR car1 = new CAR();
+      car1.setname();
+      car1.setModel();
+      car1.setMaxSpeed();
+      car1.setPrice();
 
-      car1.setname("laguna");
-      car1.setModel(1995);
-    System.out.println("thnx ");
       System.out.println(car1.getname());
       System.out.println(car1.getModel());
+      System.out.println(car1.getMaxSpeed());
+      System.out.println(car1.getPrice());
     }
 }
